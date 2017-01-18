f375771d30713bbc8b80eecd326be08d926598f5
hotshotGG <100332338@qq.com>
2017-01-18 17:33:30 +0800

    Update

---
+++ app/src/main/java/io/netopen/hotbitmapgg/databindingdemo/TestActivity.java
@@ -0,0 +1,21 @@
+package io.netopen.hotbitmapgg.databindingdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hcc on 2017/1/17 14:12
+ * 100332338@qq.com
+ * DatabindingDemo
+ *
+ * @HotBitmapGG
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+}
